e4b9eb5b92de25aa09b02b986df6a1c2af740ee1
riakau <158500784+riakau@users.noreply.github.com>
2025-03-01 00:05:03 -0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -3,25 +3,36 @@
 #include <mutex>
 #include "Account.h"
 #include <vector>
-using namespace std;
-#define num_of_customer_transactions 10
 
-class Customer{
-    private:
-        int id;
-    public:
-        Account account;
-    Customer(){
+using namespace std;
+
+#define num_of_customer_transactions 10 // Number of transactions each customer performs
+
+// Class representing a Customer who owns an Account
+class Customer {
+private:
+    int id; // Unique customer ID
+
+public:
+    Account account; // Each customer has an associated account
+
+    // Constructor to initialize customer ID
+    Customer() {
+        static int nextId = 1; // Static variable to assign unique IDs
+        id = nextId++;
     }
-    void start(){
-        for (int i=0; i < num_of_customer_transactions; i++){
-            int x = rand();
-            double y = double(x);
-            if(x%2){
-            account.deposit(x%50);
-        } 
-        else{ account.withdrawl(5.50);}
-    
+
+    // Function to simulate customer transactions
+    void start() {
+        for (int i = 0; i < num_of_customer_transactions; i++) {
+            int x = rand(); // Generate a random number
+            double y = double(x); // Convert to double (unused in logic)
+            
+            if (x % 2) { // If x is odd, deposit a random amount up to 50
+                account.deposit(x % 50);
+            } else { // If x is even, withdraw a fixed amount
+                account.withdrawl(5.50);
+            }
+        }
     }
-    }
-    };
+};
